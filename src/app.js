19d2f30ee582de1c299ad6568bb9ebe4901835b7
elizabeth.torres <elizabethtorres@ilikedesi.com>
2020-05-26 13:23:35 -0700

    removed dummy expense data

--- src/app.js
+++ src/app.js
@@ -12,12 +12,8 @@ import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
-store.dispatch(addExpense({  description: 'Water Bill', amount: 4500, createdAt: 4500})); //, createdAt: 4500 
-store.dispatch(addExpense({  description: 'Gas Bill', amount: 300, createdAt: 1000 }));  // amount: 300, 
-store.dispatch(addExpense({  description: 'Rent', amount: 109500, createdAt: 2000 }));  //, createdAt: 1000
-
 const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+
 console.log(visibleExpenses);
 
 const jsx = (
